b632ce85239c65f85791281cd1aacf766eacba1f
nora <sherlockwh@gmail.com>
2018-05-25 00:47:34 +0800

    lesson3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movemnet = new Vector2 (moveHorizontal,moveVertical);
+		rb2d.AddForce(movemnet * speed);
+	}
+}
